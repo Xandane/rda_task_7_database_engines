715b7d3c67ca3ae98d5ed6005e62ada6be05d41b
Yuriy Mishchuk <yuramishchuk1703@gmail.com>
2026-04-26 17:23:25 +0300

    fixed engines

--- task.sql
+++ task.sql
@@ -1,40 +1,35 @@
 CREATE DATABASE ShopDB; 
 USE ShopDB; 
 
--- Create a table to store countries 
 CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
 
--- Create a table for caching GeoIP data (Columns: ID, IP Range, CountryID)
-CREATE TABLE GeoIPCache(
-ID INT NOT NULL AUTO_INCREMENT,
-IPRANGE INT NOT NULL,
-COUNTRYID INT NOT NULL,
-PRIMARY KEY (ID)
+CREATE TABLE GeoIPCache (
+    ID INT NOT NULL AUTO_INCREMENT,
+    IPRange VARCHAR(50) NOT NULL,
+    CountryID INT NOT NULL,
+    PRIMARY KEY (ID)
 ) ENGINE=MEMORY;
--- Create a table for storing product descriptions for different countries (Columns: ID, CountryID, ProductID, Description )
-CREATE TABLE ProductDescription(
-ID INT,
-DESCRIPTION VARCHAR(255),
-PRODUCTID INT,
-COUNTRYID INT,
-PRIMARY KEY (ID)
-) ENGINE=MyISAM;
--- Create a table for storing logs. For now we don't need to save them, but we need to implement functionality (Columns: ID, Time, LogRecord)
-CREATE TABLE Logs(
-ID INT NOT NULL ,
-`Timestamp` TIME,
-Message VARCHAR(255),
-PRIMARY KEY (ID)
-) ENGINE=Blackhole ;
 
-
--- Create a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
-CREATE TABLE ProductReporting(
-`Date` DATE NOT NULL,
-ProductName VARCHAR(100) NOT NULL,
-ORDERS INT NOT NULL
+CREATE TABLE ProductDescription (
+    ID INT AUTO_INCREMENT,
+    Description TEXT,
+    ProductID INT,
+    CountryID INT,
+    PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
+
+CREATE TABLE Logs (
+    ID INT NOT NULL,
+    `Timestamp` TIMESTAMP,
+    Message TEXT
+) ENGINE=BLACKHOLE;
+
+CREATE TABLE ProductReporting (
+    `Date` DATE NOT NULL,
+    ProductName VARCHAR(100) NOT NULL,
+    Orders INT NOT NULL
+) ENGINE=CSV;
